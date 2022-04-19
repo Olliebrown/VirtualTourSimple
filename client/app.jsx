1646c4b063cc2942af3e98ce96c50fca1b4ae17d
Seth Berrier <berriers@uwstout.edu>
2022-04-19 06:13:50 -0500

    Solved MacOS / Safari specific issues

--- client/app.jsx
+++ client/app.jsx
@@ -6,12 +6,14 @@ import ErrorFallback from './components/ErrorFallback.jsx'
 // import ThreeFiberExample from './ThreeFiberExample.jsx'
 import VirtualTour from './VirtualTour.jsx'
 
+// Detect if motion controls are available
+let waitingForMotion = true
 let allowMotion = false
 
 // Mount the main app component
 const reactRoot = createRoot(document.getElementById('root'))
-
 function doRender () {
+  console.log('Rendering the root react element ...')
   reactRoot.render(
     <ErrorBoundary FallbackComponent={ErrorFallback}>
       <VirtualTour isMobile={false} allowMotion={allowMotion} />
@@ -21,14 +23,26 @@ function doRender () {
 
 // Detect gyro capabilities
 const onMotion = (e) => {
+  waitingForMotion = false
   window.removeEventListener('devicemotion', onMotion)
   if (e.rotationRate.alpha || e.rotationRate.beta || e.rotationRate.gamma) {
     // Turn on motion, remove this listener, and re-render
-    // if (_DEV_) alert('gyro motion detected, enabling')
+    if (_DEV_) console.log('gyro motion detected, enabling')
     allowMotion = true
     doRender()
   } else {
-    // if (_DEV_) alert('no gyro motion detected')
+    if (_DEV_) console.log('no gyro motion detected')
+    doRender()
+  }
+}
+
+// Allow timeout while waiting for a motion event
+function motionTimeout () {
+  // Stop waiting for a motion event (took too long)
+  if (waitingForMotion) {
+    console.log('Timed out while waiting for a motion event (assume not supported)')
+    waitingForMotion = false
+    window.removeEventListener('devicemotion', onMotion)
     doRender()
   }
 }
@@ -39,21 +53,23 @@ function installMotionHandler (e) {
   e.preventDefault()
   document.getElementById('permissionLink').removeEventListener('click', installMotionHandler)
 
-  // if (_DEV_) alert('Installing permission handler')
+  if (_DEV_) console.log('Installing permission handler')
   // Do we need to request permission?
-  if (typeof DeviceMotionEvent.requestPermission === 'function') {
-    // if (_DEV_) alert('Requesting permission')
+  if (typeof DeviceMotionEvent !== 'undefined' && typeof DeviceMotionEvent.requestPermission === 'function') {
+    if (_DEV_) console.log('Requesting permission')
     DeviceMotionEvent.requestPermission()
       .then(permissionState => {
         if (permissionState === 'granted') {
-          // if (_DEV_) alert('permission granted')
+          if (_DEV_) console.log('permission granted')
           window.addEventListener('devicemotion', onMotion)
+          setTimeout(motionTimeout, 500)
         }
       })
       .catch(console.error)
   } else {
-    // if (_DEV_) alert('Permission not needed')
+    if (_DEV_) console.log('Permission not needed')
     window.addEventListener('devicemotion', onMotion)
+    setTimeout(motionTimeout, 500)
   }
 }
 
