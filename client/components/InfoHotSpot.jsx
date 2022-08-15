9e4cd668508ee16d8351b3643a37cd91c39d4364
Seth Berrier <berriers@uwstout.edu>
2022-08-15 06:25:14 -0500

    Small progress in video cropping

---
+++ client/components/InfoHotSpot.jsx
@@ -0,0 +1,80 @@
+import React, { useEffect } from 'react'
+import PropTypes from 'prop-types'
+
+import { useLoader, useGraph } from '@react-three/fiber'
+import { MathUtils, TextureLoader } from 'three'
+import { OBJLoader } from 'three/examples/jsm/loaders/OBJLoader.js'
+
+import HEATING_PLANT_IMAGES from './heatingPlantImages.js'
+import useStore from '../state/useStore.js'
+
+// Various colors for the texture loading state
+const LOADING_COLOR = 0x777777
+const LOADED_COLOR = 0x156289
+const FAILED_COLOR = 0x883333
+
+export default function Arrow (props) {
+  // Destructure props
+  const { height, distance, direction, destination, ...rest } = props
+
+  // Global texture loader status and pano image state
+  const { setPano, loadingStatus } = useStore(state => state)
+
+  // Extract status for this specific image
+  const imageURL = HEATING_PLANT_IMAGES[destination]?.filename
+  const imageLoadingStatus = loadingStatus[imageURL || 'unknown']
+
+  // Lookup the texture needed for the destination and get it pre-loading
+  useEffect(() => {
+    useLoader.preload(TextureLoader, HEATING_PLANT_IMAGES[destination]?.filename)
+  }, [destination])
+
+  // Click callback function
+  const onClick = () => {
+    if (destination) { setPano(destination) }
+  }
+
+  // Load the arrow geometry and clone our own instance
+  const loadedObj = useLoader(OBJLoader, 'geom/arrow.obj')
+  const { nodes } = useGraph(loadedObj.clone())
+
+  let color = LOADING_COLOR
+  switch (imageLoadingStatus) {
+    case 'DONE':
+      color = LOADED_COLOR
+      break
+    case 'FAILED':
+      color = FAILED_COLOR
+      break
+  }
+
+  // Build unique sub-meshes for all the loaded objects
+  const meshes = Object.keys(nodes).map((meshName) => (
+    <mesh key={`${meshName}-mesh`} geometry={nodes[meshName].geometry}>
+      <meshPhongMaterial color={color} flatShading />
+    </mesh>
+  ))
+
+  // Pack in groups to position in the scene
+  return (
+    <group rotation-y={MathUtils.degToRad(direction)} {...rest} onClick={onClick}>
+      <group position={[0, height, distance]} rotation-x={Math.PI / 2.0} rotation-z={Math.PI}>
+        {meshes}
+      </group>
+    </group>
+  )
+}
+
+Arrow.propTypes = {
+  height: PropTypes.number,
+  distance: PropTypes.number,
+  direction: PropTypes.number,
+  destination: PropTypes.string
+}
+
+Arrow.defaultProps = {
+  height: -2.5,
+  distance: 5,
+  direction: 0,
+  destination: ''
+}
---
+++ client/shaders/ColorShiftMaterial.js
@@ -0,0 +1,30 @@
+import * as THREE from 'three'
+
+import { extend } from '@react-three/fiber'
+import { shaderMaterial } from '@react-three/drei'
+
+const ColorShiftMaterial = shaderMaterial(
+  { time: 0, color: new THREE.Color(0.2, 0.0, 0.1) },
+
+  // vertex shader
+  /* glsl */`
+    varying vec2 vUv;
+    void main() {
+      vUv = uv;
+      gl_Position = projectionMatrix * modelViewMatrix * vec4(position, 1.0);
+    }
+  `,
+
+  // fragment shader
+  /* glsl */`
+    uniform float time;
+    uniform vec3 color;
+    varying vec2 vUv;
+    void main() {
+      gl_FragColor.rgba = vec4(0.5 + 0.3 * sin(vUv.yxx + time) + color, 1.0);
+    }
+  `
+)
+
+extend({ ColorShiftMaterial })
+export default ColorShiftMaterial
