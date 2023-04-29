e3a82413cc005baaa190bd186a822cbe26611256
Seth Berrier <berriers@uwstout.edu>
2023-04-29 15:10:18 -0500

    Zoomable Images and Videos!!

--- client/components/HotSpots/ZoomHotSpotContent.jsx
+++ client/components/HotSpots/ZoomHotSpotContent.jsx
@@ -6,6 +6,8 @@ import PropTypes from 'prop-types'
 import ReactPlayerRaw from 'react-player'
 import { Box, Stack, Typography } from '@mui/material'
 
+import ZoomableImage from '../Utility/ZoomableImage.jsx'
+
 // Peel off the default export from the react-player module (an esbuild quirk)
 const ReactPlayer = ReactPlayerRaw.default
 
@@ -14,26 +16,23 @@ export default function ZoomHotspotContent (props) {
 
   const media = (hotspotImage?.isVideo
     ? <ReactPlayer url={`${CONFIG().ZOOM_IMAGE_PATH}/${hotspotImage.src}`} controls volume={0.5} width='100%' height='' />
-    : <Box
-        component='img'
-        sx={{
-          position: 'relative',
-          objectFit: 'contain',
-          maxHeight: 'calc(100vh - 500px)'
-        }}
+    : <ZoomableImage
+        caption={caption}
         src={`${CONFIG().ZOOM_IMAGE_PATH}/${hotspotImage.src}`}
         alt={hotspotImage.alt}
       />)
 
   return (
-    <Box sx={{ p: 2, height: 'calc(100vh - 400px)' }}>
-      <Stack spacing={2} justifyContent='center' alignItems='center'>
-        {media}
-        {caption &&
-          <Typography variant="h5" component="div">
-            {caption}
-          </Typography>}
-      </Stack>
+    <Box sx={{ p: 2, height: 'calc(100vh - 400px)', overflow: (hotspotImage?.isVideo ? undefined : 'hidden') }}>
+      {hotspotImage?.isVideo
+        ? <Stack spacing={2} justifyContent='center' alignItems='center'>
+          {media}
+          {caption &&
+            <Typography variant="h5" component="div">
+              {caption}
+            </Typography>}
+          </Stack>
+        : media}
     </Box>
   )
 }
---
+++ client/components/Utility/ZoomableImage.jsx
@@ -0,0 +1,57 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import { TransformWrapper, TransformComponent } from '../../lib/custom-zoom-pan-pinch/index.esm'
+
+import { Button, Stack, Typography } from '@mui/material'
+import { ZoomIn as ZoomInIcon, ZoomOut as ZoomOutIcon } from '@mui/icons-material'
+
+const Controls = ({ zoomIn, zoomOut, resetTransform, centerView }) => (
+  <Stack
+    direction='row'
+    spacing={1}
+    justifyContent='left'
+    sx={{ position: 'absolute', zIndex: 2, top: 10, left: 10 }}
+  >
+    <Button variant='contained' onClick={() => zoomIn()}><ZoomInIcon /></Button>
+    <Button variant='contained' onClick={() => zoomOut()}><ZoomOutIcon /></Button>
+    <Button variant='contained' onClick={() => resetTransform()}>{'Reset'}</Button>
+    <Button variant='contained' onClick={() => centerView()}>{'Center'}</Button>
+  </Stack>
+)
+
+Controls.propTypes = {
+  zoomIn: PropTypes.func.isRequired,
+  zoomOut: PropTypes.func.isRequired,
+  resetTransform: PropTypes.func.isRequired,
+  centerView: PropTypes.func.isRequired
+}
+
+export default function ZoomableImage (props) {
+  const { caption, src, alt } = props
+
+  return (
+    <TransformWrapper initialScale={1}>
+      {(utils) => (
+        <div style={{ position: 'relative' }}>
+          <Controls {...utils} />
+          <TransformComponent wrapperClass='zoomable-wrapper' contentClass='zoomable-content'>
+            <img src={src} alt={alt} />
+            <Typography variant="h5" component="div">{caption}</Typography>
+          </TransformComponent>
+        </div>
+      )}
+    </TransformWrapper>
+  )
+}
+
+ZoomableImage.propTypes = {
+  caption: PropTypes.string,
+  src: PropTypes.string.isRequired,
+  alt: PropTypes.string
+}
+
+ZoomableImage.defaultProps = {
+  caption: '',
+  alt: 'unknown image'
+}
